ba2a4ee587b6b206014d275a6b12a692b39b4cdc
Residevil <43865235+Residevil@users.noreply.github.com>
2020-11-14 16:28:40 -0800

    Update db.php

--- UserRegistration/config/db.php
+++ UserRegistration/config/db.php
@@ -13,42 +13,35 @@ $conn = new mysqli(DB_HOST, DB_USER, DB_PASS) or die ('Database error: ' . $conn
 $db = "CREATE DATABASE INFO2413";
 $conn->query($db);
 
-// sql to create table. Create
-$users = "CREATE TABLE users (
-    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-    username VARCHAR20) NOT NULL , 
-    email VARCHAR(50) NOT NULL UNIQUE, 
-    verified TINYINT(255) NOT NULL, 
-    token VARCHAR(100) NOT NULL, 
-    password VARCHAR(100) NOT NULL,
-    usertype_id INT(11) NOT NULL FOREIGN KEY REFERENCE user_type (id)
-)";
-$conn->query($users)
+if ($conn->query($db) === TRUE) {
+  // sql to create table.
+        $users = "CREATE TABLE users (
+            id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+            username VARCHAR20) NOT NULL , 
+            email VARCHAR(50) NOT NULL UNIQUE, 
+            verified TINYINT(255) NOT NULL, 
+            token VARCHAR(100) NOT NULL, 
+            password VARCHAR(100) NOT NULL,
+            usertype_id INT(11) NOT NULL FOREIGN KEY REFERENCE user_type (id)
+        )";
+        $conn->query($users)
 
-$user_type = "CREATE TABLE user_type (
-    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,  
-    usertype VARCHAT(50) NOT NULL
-)";
-$conn->query($user_type);
+        $user_type = "CREATE TABLE user_type (
+            id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,  
+            usertype VARCHAT(50) NOT NULL
+        )";
+        $conn->query($user_type);
 
-$herbs = "CREATE TABLE herbs (
-    herb_id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-    herb_name INT(20) NOT NULL ,
-    Image BIN(10) NOT NULL, 
-    herb_description VARCHAT(100) NOT NULL,
-    medical_use VARCHAR (255) NOT NULL,
-    sample_formula VARCHAR (100) NOT NULL
-)";
-$conn->query($herbs);
-
-
-
-
-
-if ($conn->query($sql) === TRUE) {
-  echo "Table MyGuests created successfully";
-} else {
-  echo "Error creating table: " . $conn->error;
+        $herbs = "CREATE TABLE herbs (
+            herb_id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+            herb_name INT(20) NOT NULL ,
+            Image BIN(10) NOT NULL, 
+            herb_description VARCHAT(100) NOT NULL,
+            medical_use VARCHAR (255) NOT NULL,
+            sample_formula VARCHAR (100) NOT NULL
+        )";
+        $conn->query($herbs);
 }
+        else { echo "Error creating table: " . $conn->error;}
 
 ?>
