6fcd9cf166acf52519bea197018d2d01e9381413
Residevil <43865235+Residevil@users.noreply.github.com>
2020-11-30 02:52:12 -0800

    Update login.php

--- UserRegistration/login.php
+++ UserRegistration/login.php
@@ -5,6 +5,9 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <script type="text/javascript" src="dist/jquery.tabledit.js"></script>
     <title>Login</title>
 </head>
 <body>
