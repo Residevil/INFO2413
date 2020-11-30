0d9c8c708875296b65031563b86a94e7f1d385f3
Residevil <43865235+Residevil@users.noreply.github.com>
2020-11-30 03:08:04 -0800

    Create INFO2413DB.sql

---
+++ INFO2413DB.sql
@@ -0,0 +1,19 @@
+CREATE TABLE users (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    username VARCHAR(50),
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(255),
+    usertype VARCHAR(20) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE EmployeeType (
+    herb_id INT(11) NOT NULL AUTO_INCREMENT,
+    herb_name VARCHAR(50) NOT NULL,
+    symptoms VARCHAR(255),
+    medicinal_uses VARCHAR(1000),
+    image VARCHAR(100),
+    botanical_description VARCHAR(1000),
+    sample_formula VARCHAR(1000),
+    PRIMARY KEY(herb_id)
+);
