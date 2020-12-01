127fddb2dff9b535aaba3334b9cdaf77e7f3897a
Residevil <43865235+Residevil@users.noreply.github.com>
2020-11-30 21:29:40 -0800

    Update SearchEngine.php

--- UserRegistration/Controllers/SearchEngine.php
+++ UserRegistration/Controllers/SearchEngine.php
@@ -6,12 +6,12 @@ require_once 'config/db.php';
 
 if(isset($_POST['search-btn'])) {
     $search = $_POST['search'];
-    //$search = preg_replace("#[^0-9a-z\s]#i","",$search);
+    $search = preg_replace("#[^0-9a-z\s]#i","",$search);
 
     $words = explode(' ', $search);
     $regex = implode('|', $words);
 
-    $query = "SELECT * FROM herbs WHERE herb_name = '". $search ."'";
+    $query = "SELECT * FROM herbs WHERE herb_name REGEXP '{$regex}' OR symptoms REGEXP '{$regex}' OR medicinal_uses REGEXP '{$regex}' OR botanical_description  REGEXP '{$regex}' OR sample_formula REGEXP '{$regex}'";
     $result = $conn->query($query) or die($conn->error);
     $count = $result->num_rows;
     if($count == 0 || empty($search)){
@@ -23,11 +23,32 @@ if(isset($_POST['search-btn'])) {
             $_SESSION['herb_id'] = $row['herb_id'];
             $_SESSION['herb_name'] = $row['herb_name'];
             $_SESSION['symptoms'] = $row['symptoms'];
-            $_SESSION['medical_use'] = $row['medical_use'];
-            $_SESSION['herb_description'] = $row['herb_description'];
+            $_SESSION['medicinal_uses'] = $row['medicinal_uses'];
+            $_SESSION['botanical_description'] = $row['botanical_description'];
             $_SESSION['sample_formula'] = $row['sample_formula'];	
-            $_SESSION['image_name'] = "<img src='images/".$row['image_name']."' width=480 height=270 >"; 
+            $_SESSION['image'] = "<img src='images/".$row['image']."' width=480 height=270 >"; 
         }
     }
 }
 
+if(isset($_POST['user-btn'])) {
+    $search = $_POST['search'];
+
+    $query = "SELECT * FROM users WHERE username=? OR email=? ";
+    $stmt = $conn->prepare($query) or die($conn->error);
+    $stmt->bind_param('ss', $search, $search);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $count = $result->num_rows;
+    if($count == 0 || empty($search)){
+        $output = 'There are zero results';
+    } else{
+        $table='';
+        $hidden = '';
+        while($row = $result->fetch_array()) {
+            $id = $row['id'];
+            $username = $row['username']; 
+            $email = $row['email'];
+        }
+    }
+}
