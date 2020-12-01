a9c2c448417e8b98d51219fbd80d32b5b26e902f
Residevil <43865235+Residevil@users.noreply.github.com>
2020-12-01 00:05:03 -0800

    Update edit_delete.php

--- UserRegistration/edit_delete.php
+++ UserRegistration/edit_delete.php
@@ -1,7 +1,16 @@
 <?php 
 require_once 'config/db.php';
+require_once 'Controllers/ModifyHerbController.php';
 require_once 'Controllers/authController.php';
 require_once 'Controllers/SearchEngine.php';
+
+if($_SESSION['edit'] == "user") {
+    $H = "hidden";
+}
+
+if($_SESSION['edit'] == "herb") {
+    $U = "hidden";
+}
 
 if(isset($_POST['add'])) {
     header('location: add.php');
@@ -9,7 +18,18 @@ if(isset($_POST['add'])) {
 
 if(isset($_POST['edit-btn'])) {
     header('location: edit.php');
+}
 
+if(isset($_POST['addUser'])) {
+    $title = "add";
+    $button = "add";
+    header('location: registration.php');
+}
+
+if($_SESSION['usertype_id'] == 1) {
+    $editU = 'visible';
+} else {
+    $editU = 'hidden';
 }
 
 if(isset($_POST['delete-btn'])) {
@@ -19,6 +39,21 @@ if(isset($_POST['delete-btn'])) {
     $stmt->bind_param('i', $herb_id);
     $stmt->execute();
     $_SESSION['message'] = "Herb is deleted successfully.";
+    $_SESSION['alert-class'] = "alert-success";
+    header('location: index.php');
+    exit();
+}
+
+if(isset($_POST['editUser'])) {
+    header('location: editUser.php');
+}
+
+if(isset($_POST['deleteUser'])) {
+    $delete = "DELETE FROM users WHERE id=?";
+    $stmt=$conn->prepare($delete);
+    $stmt->bind_param('i', $id);
+    $stmt->execute();
+    $_SESSION['message'] = "User is deleted successfully.";
     $_SESSION['alert-class'] = "alert-success";
     header('location: index.php');
     exit();
@@ -43,7 +78,7 @@ if(isset($_POST['delete-btn'])) {
     </div>
     <div class="container">
     <div class="row">
-        <div class="col-lg-8 offset-lg-2 form-div login">
+        <div class="col-lg-8 offset-lg-2 form-div login" <?php echo $H;?>>
             
             <?php if(isset($_SESSION['message'])) : ?>
                 <div class="alert <?php echo $_SESSION['alert-class']; ?>">
@@ -57,14 +92,14 @@ if(isset($_POST['delete-btn'])) {
             
             <form action="edit_delete.php" method="post">
                 <div>
-                    <button name="add" style="float: right; visibility: <?php echo $edit; ?>;">Add Herb</button>          
+                    <button name="add" style="float: right; visibility: <?php echo $editH; ?>;">Add Herb</button>          
                 </div>
             </form>
   
             <a href="index.php?logout=1" class="logout">logout</a>
             
             <div class="alert alert-warning">
-                Search for the Herb you wish to edit or delete:
+                Search for the herb you wish to edit or delete:
             </div>
                 
             <form action="edit_delete.php" method="post">
@@ -92,7 +127,65 @@ if(isset($_POST['delete-btn'])) {
                         <td><?php echo $_SESSION['botanical_description']; ?></td>
                         <td>
                             <button type="submit" name="edit-btn">Edit</button>
-                            <button type="submit" name="delete-btn"  onclick="return confirm('Are you sure you want to delete this item?');"> Delete</button></td>   
+                            <button type="submit" name="delete-btn"  onclick="return confirm('Are you sure you want to delete this Herb?');"> Delete</button></td>   
+                        </td>   
+                        </tr>
+                </tbody>
+                <?php endif ?>
+            </table>
+            </form>
+        </div>
+
+        <div class="col-lg-8 offset-lg-2 form-div login" <?php echo $U; ?>>
+            
+            <?php if(isset($_SESSION['message'])) : ?>
+                <div class="alert <?php echo $_SESSION['alert-class']; ?>">
+                    <?php 
+                        echo $_SESSION['message']; 
+                        unset($_SESSION['message']);
+                        unset($_SESSION['alert-class']);
+                    ?>
+                </div>
+            <?php endif ?>
+            
+            <form action="edit_delete.php" method="post">
+                <div>
+                    <button name="addUser" style="float: right; visibility: <?php echo $editH; ?>;">Add User</button>          
+                </div>
+            </form>
+  
+            <a href="index.php?logout=1" class="logout">logout</a>
+            
+            <div class="alert alert-warning">
+                Search for the user you wish to edit or delete:
+            </div>
+                
+            <form action="edit_delete.php" method="post">
+
+                <div>
+                    <input type="text" name="search" class="form-control form-control-lg">
+                    <button type="search" name="user-btn">Search</button>
+                </div> 
+
+            <table class="table table-striped" <?php echo $table; ?>>
+                <th>
+                    <tr>
+                        <th>UserName</th>
+                        <th style="text-align: center;">Email</th>
+                        <th></th>
+                    </tr>
+                </th>
+                <?php if(!empty($table)) {
+                    echo $output;
+                } ?>
+                <?php if(empty($hidden)) : ?>
+                <tbody> 
+                        <tr id="<?php echo $_SESSION['edit_id']; ?>">
+                        <td><?php echo $_SESSION['edit_username']; ?></td>
+                        <td><?php echo $_SESSION['edit_email']; ?></td>
+                        <td>
+                            <button type="submit" name="editUser">Edit</button>
+                            <button type="submit" name="deleteUser"  onclick="return confirm('Are you sure you want to delete this User?');"> Delete</button></td>   
                         </td>   
                         </tr>
                 </tbody>
